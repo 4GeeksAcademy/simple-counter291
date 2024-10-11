15dc29822d9a2eca3c8f3302a5cfae8f1a3a3e5e
Jyebenes <131881275+Jyebenes@users.noreply.github.com>
2024-10-11 21:51:39 +0000

    simple counter listo

---
+++ Simple-counter/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import SecondsCounter from './componentes/SecondsCounter';
+
+
+const App = () => {
+  return (
+
+   <SecondsCounter seconds={3434} />
+   
+  );
+}
+
+  
+  
+
+
+export default App
---
+++ Simple-counter/src/componentes/SecondsCounter.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+
+
+
+const SecondsCounter = (props) => {
+  const [secondsCounter, setSecondsCounter] = useState(0)
+ console.log(props)
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecondsCounter ( reset =>
+        reset >= props.seconds ? 0 : reset + 1
+      );
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [secondsCounter])
+
+
+
+
+  const dividir= String(secondsCounter).padStart(6, '0'); 
+
+  
+  return (
+    <div>
+    <h1>
+      <i className="fa-solid fa-clock"></i>
+      {dividir.split('').map((digit) => (
+        <span className="counter-digit">{digit}</span>
+      ))}
+    </h1>
+    
+    </div>
+  );
+};
+
+
+export default SecondsCounter;
